52d3f50e9df2ffa107291d883f0a18b68627fda8
trangthuyng07 <157338478+trangthuyng07@users.noreply.github.com>
2025-06-06 07:36:50 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-export {default} from "./7e634ebe314443f8@174.js";
+import define from "./7e634ebe314443f8@174.js";
+import {Runtime, Inspector} from "./runtime.js";
+
+const runtime = new Runtime();
+const main = runtime.module(define);
+
+// Gắn viewof gallery vào phần tử DOM trong index.html
+main.variable(Inspector.into("#observable-gallery")("viewof gallery"));
